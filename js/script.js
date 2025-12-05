f488af79bf757870dfe2ec6e0fc8279afd263238
ichi-seki <aka24005@byui.edu>
2025-12-05 16:41:54 -0700

    details

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// detail.js など、画像を読み込むJSファイルに追加
+const isLocal = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost';
+const basePrefix = isLocal ? '../' : ''; // ローカルなら一つ上の階層へ
+
+// 画像パスを生成する際に、このプレフィックスを付ける
+const imagePath = `${basePrefix}images/p1.jpg`;
+
 let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
